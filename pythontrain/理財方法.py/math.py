85ed7d6bff9e48786b06001c03e2aaa70f5441cc
ben <bigben83530@gmail.com>
2021-03-29 19:49:08 +0800

    v2

--- pythontrain/理財方法.py/math.py
+++ pythontrain/理財方法.py/math.py
@@ -1,14 +1,18 @@
 from openpyxl import Workbook
-from openpyxl.chart import (
-    AreaChart,
-    Reference,
-    Series,
-)
-from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.worksheet.datavalidation import DataValidation
 wb = Workbook()
-ws = wb.active
-for i in range(10):
-    ws.append([i])
+
+ws = wb.create_sheet('New Sheet')
+
+for number in range(1,50): #Generates 99 "ip" address in the Column A;
+    ws['A{}'.format(number)].value= "192.168.1.{}".format(number)
+
+data_val = DataValidation(type="list",formula1='=$A:$A') #You can change =$A:$A with a smaller range like =A1:A9
+ws.add_data_validation(data_val)
+
+data_val.add(ws["B1"]) #If you go to the cell B1 you will find a drop down list with all the values from the column A
+
+
 
 
 wb.save('test.xlsx')
